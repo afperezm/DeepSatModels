86a0295602a9bf2a0496bd0f42cbeb41d6c367c5
Andres Felipe Perez <perezmaf@myumanitoba.ca>
2025-07-12 03:47:44 -0500

    Create separate script for testing model

---
+++ train_and_eval/segmentation_test_transf.py
@@ -0,0 +1,137 @@
+from utils.lr_scheduler import build_scheduler
+from torch.utils.tensorboard import SummaryWriter
+from models import get_model
+from utils.config_files_utils import read_yaml, copy_yaml, get_params_values
+from utils.torch_utils import get_device, get_net_trainable_params, load_from_checkpoint
+from data import get_dataloaders
+from metrics.torch_metrics import get_mean_metrics
+from metrics.numpy_metrics import get_classification_metrics, get_per_class_loss
+from metrics.loss_functions import get_loss
+# from utils.summaries import write_mean_summaries, write_class_summaries
+from data import get_loss_data_input
+
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+
+import argparse
+import sys
+import os
+import torch
+
+sys.path.insert(0, os.getcwd())
+
+
+def test(net, eval_loader, loss_fn, num_classes):
+    predicted_all = []
+    labels_all = []
+    losses_all = []
+    net.eval()
+    with torch.no_grad():
+        for step, sample in enumerate(eval_loader):
+            logits = net(sample['inputs'].to(device))
+            logits = logits.permute(0, 2, 3, 1)
+            _, predicted = torch.max(logits.data, -1)
+            ground_truth = loss_input_fn(sample, device)
+            loss = loss_fn['all'](logits, ground_truth)
+            target, mask = ground_truth
+            if mask is not None:
+                predicted_all.append(predicted.view(-1)[mask.view(-1)].cpu().numpy())
+                labels_all.append(target.view(-1)[mask.view(-1)].cpu().numpy())
+            else:
+                predicted_all.append(predicted.view(-1).cpu().numpy())
+                labels_all.append(target.view(-1).cpu().numpy())
+            losses_all.append(loss.view(-1).cpu().detach().numpy())
+
+            # if step > 5:
+            #    break
+
+    print("finished iterating over dataset after step %d" % step)
+    print("calculating metrics...")
+    predicted_classes = np.concatenate(predicted_all)
+    target_classes = np.concatenate(labels_all)
+    losses = np.concatenate(losses_all)
+
+    eval_metrics = get_classification_metrics(predicted=predicted_classes, labels=target_classes,
+                                              n_classes=num_classes, unk_masks=None)
+
+    micro_acc, micro_precision, micro_recall, micro_F1, micro_IOU = eval_metrics['micro']
+    macro_acc, macro_precision, macro_recall, macro_F1, macro_IOU = eval_metrics['macro']
+    class_acc, class_precision, class_recall, class_F1, class_IOU = eval_metrics['class']
+
+    un_labels, class_loss = get_per_class_loss(losses, target_classes, unk_masks=None)
+
+    print(
+        "-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("Mean (micro) Evaluation metrics (micro/macro), loss: %.7f, iou: %.4f/%.4f, accuracy: %.4f/%.4f, "
+          "precision: %.4f/%.4f, recall: %.4f/%.4f, F1: %.4f/%.4f, unique pred labels: %s" %
+          (losses.mean(), micro_IOU, macro_IOU, micro_acc, macro_acc, micro_precision, macro_precision,
+           micro_recall, macro_recall, micro_F1, macro_F1, np.unique(predicted_classes)))
+    print(
+        "-----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    return (un_labels,
+            {"macro": {"Loss": losses.mean(), "Accuracy": macro_acc, "Precision": macro_precision,
+                       "Recall": macro_recall, "F1": macro_F1, "IOU": macro_IOU},
+             "micro": {"Loss": losses.mean(), "Accuracy": micro_acc, "Precision": micro_precision,
+                       "Recall": micro_recall, "F1": micro_F1, "IOU": micro_IOU},
+             "class": {"Loss": class_loss, "Accuracy": class_acc, "Precision": class_precision,
+                       "Recall": class_recall,
+                       "F1": class_F1, "IOU": class_IOU}}
+            )
+
+
+def main():
+    config_file = args.config
+    print(args.device)
+    device_ids = [int(d) for d in args.device.split(',')]
+
+    device = get_device(device_ids, allow_cpu=False)
+
+    config = read_yaml(config_file)
+    config['local_device_ids'] = device_ids
+
+    dataloaders = get_dataloaders(config)
+
+    net = get_model(config, device)
+
+    lr = float(config['SOLVER']['lr_base'])
+    save_path = config['CHECKPOINT']["save_path"]
+    checkpoint = config['CHECKPOINT']["load_from_checkpoint"]
+    local_device_ids = config['local_device_ids']
+    num_classes = config['MODEL']['num_classes']
+
+    if checkpoint:
+        load_from_checkpoint(net, checkpoint, partial_restore=False)
+
+    print("current learn rate: ", lr)
+
+    if len(local_device_ids) > 1:
+        net = nn.DataParallel(net, device_ids=local_device_ids)
+    net.to(device)
+
+    if save_path and (not os.path.exists(save_path)):
+        os.makedirs(save_path)
+
+    copy_yaml(config)
+
+    loss_fn = {'all': get_loss(config, device, reduction=None),
+               'mean': get_loss(config, device, reduction="mean")}
+
+    test_metrics = test(net, dataloaders['test'], loss_fn, num_classes)
+
+    # write_mean_summaries(writer, test_metrics[1]['micro'], abs_step, mode="test_micro", optimizer=None)
+    # write_mean_summaries(writer, test_metrics[1]['macro'], abs_step, mode="test_macro", optimizer=None)
+    # write_class_summaries(writer, [test_metrics[0], test_metrics[1]['class']], abs_step, mode="test", optimizer=None)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    parser.add_argument('--config', help='configuration (.yaml) file to use')
+    parser.add_argument('--device', default='0,1', type=str, help='gpu ids to use')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main()
